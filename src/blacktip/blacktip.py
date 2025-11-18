9fed80829934cc3845da343a59bbc4e8d19f16ab
mhawthorne-nip <mhawthorne@nipgroup.com>
2025-11-17 18:04:10 -0600

    fix: Schedule initial speed test to run after 60 seconds

--- src/blacktip/blacktip.py
+++ src/blacktip/blacktip.py
@@ -103,12 +103,12 @@ class Blacktip:
             scheduler.start()
             logger.info("Speed test scheduled to run every {} hour(s)".format(interval_hours))
             
+            # Run initial speed test after 60 seconds
             initial_run = datetime.now() + timedelta(seconds=60)
             scheduler.add_job(
                 func=lambda: speedtest_service.run_speed_test(triggered_by='scheduled'),
                 trigger='date',
-                run_date=initial_run
-                run_date=time.time() + 60,
+                run_date=initial_run,
                 id='speedtest_initial',
                 name='Initial Speed Test'
             )
